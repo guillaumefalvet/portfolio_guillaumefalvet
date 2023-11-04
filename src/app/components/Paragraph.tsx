8aa3c481a15c9b71596ddba9a4c1bb7c31832440
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-04 23:13:20 +0100

    ✨ feat : object destructuring on my prop object

--- src/app/components/Paragraph.tsx
+++ src/app/components/Paragraph.tsx
@@ -2,10 +2,10 @@ interface PropChildren {
   children: string;
 }
 
-export default function Paragraph(props: PropChildren) {
+export default function Paragraph({ children }: PropChildren) {
   return (
     <p className="text-md py-5 leading-8 mx-auto md:text-xl text-justify">
-      {props.children}
+      {children}
     </p>
   );
 }
