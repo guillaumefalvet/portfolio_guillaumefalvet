b4bafb1704b11e5760b8509a8001deac543428c1
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-05 16:02:45 +0100

    ✨ feat : added title for Paragraph comp..

--- src/app/components/Paragraph.tsx
+++ src/app/components/Paragraph.tsx
@@ -1,11 +1,15 @@
 interface PropChildren {
   children: string;
+  title: string;
 }
 
-export default function Paragraph({ children }: PropChildren) {
+export default function Paragraph({ children, title }: PropChildren) {
   return (
-    <p className="text-md py-5 leading-8 mx-auto md:text-xl text-justify">
-      {children}
-    </p>
+    <div className="p-10">
+      <h3 className="text-4xl py-2 text-center font-medium">{title}</h3>
+      <p className="text-md py-5 leading-8 mx-auto md:text-xl text-justify">
+        {children}
+      </p>
+    </div>
   );
 }
