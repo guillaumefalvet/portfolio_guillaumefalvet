6eafcf7f59f52b6e9eb8dc30487c68a9c7d196b4
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-16 16:45:38 +0100

    🔧 config : html lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Falvet Guillaume',
@@ -10,16 +10,16 @@ export const metadata: Metadata = {
   other: {
     'theme-color': '#322315',
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
