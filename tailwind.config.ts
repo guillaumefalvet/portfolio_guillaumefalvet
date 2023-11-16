ccace2e04127e72ea886bce12a520f00e1dee907
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-16 16:47:24 +0100

    🐞 fix : typo on font name

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
       },
       fontFamily: {
         samantha: 'samantha-signature',
-        satochi: 'satochi',
+        satoshi: 'satoshi',
       },
     },
     screens: {
