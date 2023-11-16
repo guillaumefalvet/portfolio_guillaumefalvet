60d07cc11ef0049be8c3106a4747f23316b93db4
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-16 21:28:08 +0100

    ✨ feat : UI components

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -8,11 +8,11 @@ type ButtonProps = {
 
 export default function Button({ children, url, submit = false }: ButtonProps) {
   const buttonClasses = [
-    // basic style
-    'from-main to-accents mb-2  me-2 mt-4 rounded-lg bg-gradient-to-r px-5 py-2.5 text-center text-sm font-medium text-white',
+    // basic style from-background to-mainColor mb-2  me-2 mt-4 rounded-lg bg-gradient-to-r
+    ' mb-2  me-2 mt-4 rounded-lg bg-white px-5 py-2.5 text-center text-sm font-medium text-black',
     // hover style
     'hover:bg-gradient-to-l',
-    'focus:ring-orange focus:outline-none focus:ring-4',
+    'focus:ring-light focus:outline-none focus:ring-4',
   ]
 
   return (
--- src/app/components/ui/H3.tsx
+++ src/app/components/ui/H3.tsx
@@ -1,5 +1,6 @@
-const H3 = ({ children }) => {
+type H3Props = {
+  children: string
+}
+export default function H3({ children }: H3Props) {
   return <h3 className="my-3 text-center text-xl font-bold">{children}</h3>
 }
-
-export default H3
