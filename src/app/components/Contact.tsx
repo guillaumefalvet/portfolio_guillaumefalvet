0b80e2b60c86fdb546e50c301f71f1f19a38dad2
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-16 21:29:22 +0100

    ⚒️ build : WIP contact

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
-import Input from '@components/Input';
-import { emailFormValidation } from '@lib/objectValidation';
+import { useState } from 'react'
+import Input from './ui/Input'
+import { emailFormValidation } from '@lib/objectValidation'
+import Button from './ui/Button'
 
 type FormData = {
-  email: string;
-  lastName: string;
-  firstName: string;
-  subject: string;
-  message: string;
-};
+  email: string
+  lastName: string
+  firstName: string
+  subject: string
+  message: string
+}
 
 const initialFormData: FormData = {
   email: '',
@@ -16,72 +17,65 @@ const initialFormData: FormData = {
   firstName: '',
   subject: '',
   message: '',
-};
+}
 export default function Contact() {
-  const [formData, setFormData] = useState<FormData>(initialFormData);
+  const [formData, setFormData] = useState<FormData>(initialFormData)
 
   function handleChange(identifier: keyof FormData, value: string) {
     setFormData((previousData) => {
       return {
         ...previousData,
         [identifier]: value,
-      };
-    });
+      }
+    })
   }
   async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
+    e.preventDefault()
     try {
-      const validatedData = emailFormValidation.parse(formData);
+      const validatedData = emailFormValidation.parse(formData)
       await fetch('/api/email', {
         method: 'POST',
         body: JSON.stringify(validatedData),
-      });
+      })
     } catch (error: any) {
-      console.log(error.message);
+      console.log(error.message)
     }
   }
 
   return (
-    <div>
-      <h2>Contact Us</h2>
-      <form onSubmit={handleSubmit} className="space-y-10">
-        <Input
-          label="email"
-          value={formData.email}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            handleChange('email', event.target.value)
-          }
-        />
-        <Input
-          label="Nom"
-          value={formData.firstName}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            handleChange('firstName', event.target.value)
-          }
-        />
-        <Input
-          label="Prénom"
-          value={formData.lastName}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            handleChange('lastName', event.target.value)
-          }
-        />
-        <Input
-          label="Sujet"
-          value={formData.subject}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            handleChange('subject', event.target.value)
-          }
-        />
+    <div className="xsm:w-screen md:w-2/4">
+      <form onSubmit={handleSubmit} className="md:mx-8">
+        <h3 className="text-accents py-2 text-center text-4xl font-medium">
+          Contact
+        </h3>
+        <div className="mb-6 grid gap-6 md:grid-cols-2">
+          <Input
+            label="email"
+            type="email"
+            value={formData.email}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleChange('email', event.target.value)
+            }
+          />
+          <Input
+            label="Sujet"
+            value={formData.subject}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleChange('subject', event.target.value)
+            }
+          />
+        </div>
         <div>
-          <label>Message:</label>
+          <label className="block">Message:</label>
           <textarea
+            className="w-full text-black"
+            rows={4}
             value={formData.message}
             onChange={(event) => handleChange('message', event.target.value)}
           ></textarea>
         </div>
-        <button type="submit">Submit</button>
+        <Button submit={true}>Envoyer</Button>
       </form>
     </div>
-  );
+  )
 }
