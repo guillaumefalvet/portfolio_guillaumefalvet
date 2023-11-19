32766d12fa69399657d23c6db240bcc70375730c
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-19 23:03:27 +0100

    🐞 fix : disabled animated bg until i improve it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,10 @@ import LandingPart from '@components/LandingPart'
 
 export default function Home() {
   return (
-    <main className="font-satoshi background-animate bg-gradient-to-br from-background to-stone-600 px-5 text-white dark:bg-background dark:text-white">
+    <main
+      className="bg-gradient-to-bl
+     from-background to-stone-600 px-5 font-satoshi text-white dark:bg-background dark:text-white"
+    >
       {/* "background-animate to-main via-backgroundx from-backgroundx bg-gradient-to-r px-4" */}
       <NavBar />
       <LandingPart />
