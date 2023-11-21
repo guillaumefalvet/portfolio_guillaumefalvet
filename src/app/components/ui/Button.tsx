35e96d9d7ba8ed68c65ec0c4080c5fb8de205065
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-21 23:14:30 +0100

    💄 style : button hover text color

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({ children, url }: ButtonProps) {
     // basic style from-background to-mainColor mb-2  me-2 mt-4 rounded-lg bg-gradient-to-r
     ' mb-2  me-2 mt-4 rounded-lg bg-white px-5 py-2.5 text-center text-sm font-medium text-black',
     // hover style
-    'hover:bg-gradient-to-l',
+    'hover:bg-gradient-to-l hover:text-light',
     'focus:ring-light focus:outline-none focus:ring-4',
   ]
 
