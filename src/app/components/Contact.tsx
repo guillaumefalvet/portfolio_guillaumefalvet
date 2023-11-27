ec8f7ad1174840aafd32fc5f9e38d00d815831d5
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-27 18:51:03 +0100

    🐞 fix : page not loading due to zod

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -3,18 +3,9 @@ import { twClsxMerge } from '@lib/utils'
 import emailAction from '../action/email/emailAction'
 import { sanitize } from 'isomorphic-dompurify'
 import H3 from './ui/H3'
-import i18next from 'i18next'
-import { z } from 'zod'
-import { zodI18nMap } from 'zod-i18n-map'
 
-import translation from 'zod-i18n-map/locales/fr/zod.json'
-i18next.init({
-  lng: 'fr',
-  resources: {
-    fr: { zod: translation },
-  },
-})
-z.setErrorMap(zodI18nMap)
+import { z } from 'zod'
+
 export type FormDataType = {
   email: string
   subject: string
