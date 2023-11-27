426474c03339bee0fc426aff9a560699b9e69cba
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-11-27 13:23:28 +0100

    ✨ feat : conditional rending based on a state... all i say

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,27 @@ import Footer from '@components/Footer'
 import AboutAndSkils from '@components/AboutAndSkils'
 import Timeline from '@components/Timeline'
 import LandingPart from '@components/LandingPart'
+import { useState } from 'react'
 import Contact from '@components/Contact'
+import HackerManPage from '@components/HackerManPage'
 
 export default function Home() {
+  const [isAHackerMan, setIsAHackerman] = useState<boolean>(false)
   return (
-    <main
-      className="bg-gradient-to-bl
-     from-background to-stone-600 px-5 font-satoshi text-white dark:bg-background dark:text-white"
-    >
-      <NavBar />
-      <LandingPart />
-      <AboutAndSkils />
-      <Projects />
-      <Timeline />
-      <Contact />
-      <Footer />
-    </main>
+    <>
+      {isAHackerMan ? (
+        <HackerManPage />
+      ) : (
+        <main className="bg-gradient-to-bl from-background to-stone-600 px-5 font-satoshi text-white dark:bg-background dark:text-white">
+          <NavBar />
+          <LandingPart />
+          <AboutAndSkils />
+          <Projects />
+          <Timeline />
+          <Contact setIsAHackerman={setIsAHackerman} />
+          <Footer />
+        </main>
+      )}
+    </>
   )
 }
